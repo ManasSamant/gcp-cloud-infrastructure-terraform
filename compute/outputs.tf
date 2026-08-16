b0006279fd1bb6a7f48cd2b7d3716ce26682dd7c
ManasSamant <92379783+ManasSamant@users.noreply.github.com>
2026-08-16 15:25:40 +0530

    Delete compute directory

--- compute/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "function_url" {
-  value = google_cloudfunctions2_function.backend.service_config[0].uri
-}
-
-output "function_name" {
-  value = google_cloudfunctions2_function.backend.name
-}
