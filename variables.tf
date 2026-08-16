f1494bb8e1603e1e073b84ea4e4d9e0d416bfdfc
ManasSamant <92379783+ManasSamant@users.noreply.github.com>
2026-08-16 15:30:02 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_connector_id" {
+  type = string
+}
+
+variable "cloud_function_sa_email" {
+  type = string
+}
+
+variable "cloud_run_sa_email" {
+  type = string
+}
